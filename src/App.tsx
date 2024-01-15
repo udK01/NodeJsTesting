877efca2615406b1042531eb9cec31af19b4a1b0
udK01 <dominik.muranyi@gmail.com>
2024-01-15 19:37:16 +0000

    Learned click events, highlights and passing values into functions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import Message from './Message';
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><Message /></div>
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  return (
+    <div>
+      <ListGroup items={items} heading={"Cities"} />
+    </div>
+  );
 }
 
 export default App;
