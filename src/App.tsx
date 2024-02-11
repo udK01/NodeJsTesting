ad4cabb90aa46825660b71a1f9c1c485fd18913e
udK01 <dominik.muranyi@gmail.com>
2024-02-11 11:40:54 +0000

    Relearnt React

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 
 function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
+  const items = [
+    `apple`,
+    `pizza`,
+    `pineapple`,
+    `coconut`,
+    `banana`,
+    `Spongebob`,
+    `Patrick`,
+  ];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
 
   return (
-    <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>Hello World</Alert>
-      )}
-      <Button onClick={() => setAlertVisibility(true)}>Click Me!</Button>
-    </div>
+    <>
+      <div>
+        {alertVisible && (
+          <Alert onClose={() => setAlertVisibility(false)}>Hello World</Alert>
+        )}
+        <Button onClick={() => setAlertVisibility(true)}>Click Me!</Button>
+      </div>
+      <div>
+        <ListGroup
+          items={items}
+          heading="Foods"
+          onSelectItem={() => handleSelectItem}
+        />
+      </div>
+    </>
   );
 }
 
